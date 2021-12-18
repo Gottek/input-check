2fff60382fb4710358042531f191790efec55f60
Amarladzi <amar.hadzimejlic@gmail.com>
2021-12-18 01:15:18 +0100

    solution finie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 
 const Post = ({ post }) => {
     return (
@@ -35,16 +35,16 @@ const PostForm = ({
                 <p>
                     {" "}
                     title:{" "}
-                    <input value={newTitle} onChange={titleChangeHandler} />
+                    <input value={newTitle} onChange={titleChangeHandler} required />
                 </p>
                 <p>
                     {" "}
                     number:{" "}
-                    <input value={newAuthor} onChange={authorChangeHandler} />
+                    <input value={newAuthor} onChange={authorChangeHandler} required />
                 </p>
                 <p>
                     {" "}
-                    url: <input value={newURL} onChange={URLChangeHandler} />
+                    url: <input value={newURL} onChange={URLChangeHandler} required />
                 </p>
                 <div>
                     <button type="submit">Add</button>
@@ -65,32 +65,55 @@ const App = () => {
     const [showAll, setShowAll] = useState(true);
     const [newFilter, setNewFilter] = useState("");
     const [newURL, setNewURL] = useState("");
+
     const addPost = event => {
         event.preventDefault();
-        const postObject = {
-            title: newTitle,
-            author: newAuthor,
-            url: newURL,
-            likes: 0,
-            id: posts.length + 1
-        };
-        if (posts.findIndex(post => post.title === newTitle) > -1) {
-            console.log("post already exists");
-            alert(`${newTitle} is already added to authorbook`);
-        } else {
-            setPosts(posts.concat(postObject));
+        if (verificationUrl(newURL) && verificationInput(newTitle) && verificationInput(newAuthor)) {
+            var titre = nettoyageInput(newTitle)
+            var author = nettoyageInput(newAuthor)
+
+            //deuxième verification
+            if (verificationInput(titre) && verificationInput(newAuthor)) {
+
+                const postObject = {
+                    title: titre,
+                    author: author,
+                    url: newURL,
+                    likes: 0,
+                    id: posts.length + 1
+                };
+                if (posts.findIndex(post => post.title === newTitle) > -1) { alert(`${newTitle} is already added to authorbook`); }
+                else { setPosts(posts.concat(postObject)); }
+            }
         }
+
+
         setNewTitle("");
         setNewAuthor("");
         setNewURL("");
     };
-    const filterChangeHandler = event => {
-        console.log(event.target.value);
-        setNewFilter(event.target.value);
-    };
-    const likeChangeHandler = event => {
-        setLikes(likes + 1);
-    };
+    function verificationInput(input) {
+        //verification
+        if (input.trim().length < 4 || input.trim().length > 40) { alert("Chaine de caractère trop long ou trop court"); return false; }
+        if (input.trim() === "" || input === null) { alert("Vous avez rentrer du vide"); return false; }
+
+        return true;
+    }
+    function nettoyageInput(input) {
+        return input.replace(/[#_@\\/<>[\]*$!&{}]/gi, '');
+    }
+
+    function verificationUrl(url) {
+        var isCorrect = true;
+        var expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+        var regex = new RegExp(expression);
+        if (!url.match(regex)) { isCorrect = false; alert("URL non-conforme au format"); }
+        return isCorrect
+    }
+    
+
+    const filterChangeHandler = event => { setNewFilter(event.target.value); };
+    const likeChangeHandler = event => { setLikes(likes + 1); };
 
     const rows = () =>
         posts
@@ -99,18 +122,9 @@ const App = () => {
             )
             .map(post => <Post key={post.id} post={post} />);
 
-    const titleChangeHandler = event => {
-        console.log(event.target.value);
-        setNewTitle(event.target.value);
-    };
-    const authorChangeHandler = event => {
-        console.log(event.target.value);
-        setNewAuthor(event.target.value);
-    };
-    const URLChangeHandler = event => {
-        console.log(event.target.value);
-        setNewURL(event.target.value);
-    };
+    const titleChangeHandler = event => { setNewTitle(event.target.value); };
+    const authorChangeHandler = event => { setNewAuthor(event.target.value); };
+    const URLChangeHandler = event => { setNewURL(event.target.value); };
 
     return (
         <div>
